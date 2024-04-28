ec873311f29dd876602f2f731d104879ad2cd68f
Łukasz Bacik <mail@luka.sh>
2024-04-28 13:14:54 +0200

    Add recursively param (entity collection)

--- src/SDK/EntityCollection.php
+++ src/SDK/EntityCollection.php
@@ -10,10 +10,10 @@ class EntityCollection extends ObjectCollection
 {
     protected static string $type = Entity::class;
 
-    public function toArray(): array
+    public function toArray(bool $recursively = true): array
     {
         return array_map(
-            fn (Entity $entity) => (array) $entity,
+            fn (Entity $entity) => $recursively ? (array) $entity : $entity,
             $this->getIterator()->getArrayCopy(),
         );
     }
